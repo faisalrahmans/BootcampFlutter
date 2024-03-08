d6024328d3805a1de928d3806952b71edcd5afb0
faisalrahsu_bankraya <faisal.sulaswanto@bankraya.co.id>
2024-03-09 00:02:25 +0700

    Tugas 15 Routing Management

---
+++ sanberappflutter/lib/Latihan/Latihan-2/pages/page_3.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:sanberappflutter/Latihan/Latihan-2/routes/route_name.dart';
+
+class PageThree extends StatelessWidget {
+  const PageThree({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Halaman 3"),
+            ElevatedButton(
+              onPressed: () {
+                Get.toNamed(RouteName.page_2);
+              },
+              child: Text("Kembali ke halaman dua"),
+            ), // ElevatedButton
+          ],
+        ), // Column
+      ), // Center
+    ); // Scaffold
+  }
+}
--- sanberappflutter/lib/main.dart
+++ sanberappflutter/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:sanberappflutter/Latihan/Latihan-2/pages/page_1.dart';
+import 'package:sanberappflutter/Latihan/Latihan-2/routes/page_routes.dart';
 // import 'package:sanberappflutter/Latihan/Latihan-1/get_data_screen.dart';
 // import 'package:sanberappflutter/Latihan/get-x/counter_screen.dart';
 // import 'package:sanberappflutter/Tugas/Tugas12/get_data.dart';
@@ -7,6 +9,7 @@ import 'package:sanberappflutter/Tugas/Tugas13/LoginScreen.dart';
 // import 'package:sanberappflutter/Tugas/Tugas12/get_data.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:get/get.dart';
 // import 'package:sanberappflutter/Tugas/Tugas10/Home.dart';
 // import 'package:sanberappflutter/Tugas/Tugas10/pages/HomeScreen.dart';
 // import 'package:sanberappflutter/Tugas/Tugas10/pages/HomeScreen.dart';
@@ -25,10 +28,12 @@ Future<void> main() async {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: LoginScreen(),
+      home: PageOne(),
+      getPages:
+          RouteArr.pages, // Use the PageRouteApp class from page_routes.dart
     );
   }
 }
